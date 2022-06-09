6b796efee5c632a4b333bb680de4512a5e85c62b
qiye <1181362579@qq.com>
2022-06-09 11:43:00 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="box">
+    <h1>{{ msg }} 1111</h1>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.box {
+
+}
+</style>
